c005107e33481fbac6a585ac9638a9ac1038dc01
YiLiu6240 <yi.liu6240@gmail.com>
2020-09-17 11:58:42 +0100

    Add data dictionary schema

--- epigraphdb_data_dicts.py
+++ epigraphdb_data_dicts.py
@@ -3,9 +3,32 @@ from pathlib import Path
 from typing import Dict, List, Optional, Union
 
 import yaml
+from pydantic import BaseModel
 
 META_NODES_FILE = Path(__file__).parent / "data-dict-meta-nodes.yml"
 META_RELS_FILE = Path(__file__).parent / "data-dict-meta-rels.yml"
+
+
+class PopulatedProperty(BaseModel):
+    "The property field populated."
+    doc: str
+
+
+class DataDictNodeEntity(BaseModel):
+    "Meta node entity as specified by the data dict."
+    id: Optional[str]
+    name: Optional[str]
+    doc: Optional[str]
+    properties: List[Union[str, Dict[str, PopulatedProperty]]]
+
+
+class DataDictRelEntity(BaseModel):
+    "Meta relationship entity as specified by the data dict."
+    source: str
+    target: str
+    doc: Optional[str]
+    properties: Optional[List[Union[str, Dict[str, PopulatedProperty]]]]
+
 
 with META_NODES_FILE.open("r") as f:
     meta_nodes_dict = yaml.safe_load(f)
@@ -41,12 +64,12 @@ def get_property_docs(
         return res
 
 
-meta_nodes_docs = {
+meta_nodes_property_docs = {
     key: get_property_docs(value["properties"])
     for key, value in meta_nodes_dict.items()
 }
 
-meta_rels_docs = {
+meta_rels_property_docs = {
     key: get_property_docs(value["properties"])
     for key, value in meta_rels_dict.items()
 }
--- tests/test_epigraphdb_data_dicts.py
+++ tests/test_epigraphdb_data_dicts.py
@@ -1,15 +1,33 @@
 import pytest
 
 from epigraphdb_common_utils.epigraphdb_data_dicts import (
-    meta_nodes_docs,
-    meta_rels_docs,
+    DataDictNodeEntity,
+    DataDictRelEntity,
+    meta_nodes_dict,
+    meta_nodes_property_docs,
+    meta_rels_dict,
+    meta_rels_property_docs,
 )
 
 
-@pytest.mark.parametrize("docs_data", [meta_nodes_docs, meta_rels_docs])
+@pytest.mark.parametrize(
+    "docs_data", [meta_nodes_property_docs, meta_rels_property_docs]
+)
 def test_docs(docs_data):
     assert isinstance(docs_data, dict)
     for key, value in docs_data.items():
         if isinstance(value, dict):
             for value_key, value_value in value.items():
                 assert isinstance(value_value, str) or value_value is None
+
+
+def test_node_dict():
+    for key, value in meta_nodes_dict.items():
+        print(key)
+        assert DataDictNodeEntity(**value)
+
+
+def test_rel_dict():
+    for key, value in meta_rels_dict.items():
+        print(key)
+        assert DataDictRelEntity(**value)
